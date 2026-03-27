83a8424b0bcc755743c7d21a49af04072e554244
jvn <73175916+JevenDev@users.noreply.github.com>
2026-03-27 02:27:00 -0400

    deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const isUserOrOrgPage = repoName.endsWith('.github.io');
+const base = isGitHubActions && repoName && !isUserOrOrgPage ? `/${repoName}/` : '/';
+
 export default defineConfig({
+  base,
   plugins: [vue()],
   build: {
     rollupOptions: {
